5e6479523f69f2a6e14603f71c27cc7c8f67b49d
Hugo Muñoz <hugo.m.steenman@gmail.com>
2025-04-17 12:06:38 +0200

    Add files via upload

--- ball.py
+++ ball.py
@@ -1,15 +1,22 @@
 # ball.py
 
 class Ball:
-    def __init__(self, radius, color, position, bounce):
+    def __init__(self, radius, color, position, bounce, weight):
         self.radius = radius
         self.color = color
         self.position = position
         self.bounce = bounce
-        
+        self.min_weight = 0.1
+        self.max_weight = 5
+        self.weight = self.set_weight(weight)
 
+    def set_weight(self, weight):
+        if weight < self.min_weight or weight > self.max_weight:
+            print("Invalid weight. Weight should be between", self.min_weight, "and", self.max_weight)
+            return None
+        return weight
     def change_color(self, new_color):
         self.color = new_color
 
     def __str__(self):
-        return f"Ball(radius={self.radius}, color='{self.color}', position={self.position}, bounce={self.bounce})"
+        return f"Ball(radius={self.radius}, color='{self.color}', position={self.position}, bounce={self.bounce}, weight={self.weight})"
--- main.py
+++ main.py
@@ -3,16 +3,17 @@
 from ball import Ball
 
 # Create a small red ball
-small_ball = Ball(radius=5, color="red", bounce=0.8, position=(0, 2))
+small_ball = Ball(radius=5, color="red", bounce=0.8, position=(0, 2), weight=2)
 
 # Create a big blue ball
-big_ball = Ball(radius=15, color="blue", bounce=0.5, position=(5, 7))
+big_ball = Ball(radius=15, color="blue", bounce=0.5, position=(5, 7), weight=7)
 
 # Print their details
 print("Small Ball:", small_ball)
 print("Big Ball:", big_ball)
 
-
 # Let's change the color of the small ball
+"""
 small_ball.change_color("yellow")
 print("Small Ball after color change:", small_ball)
+"""
